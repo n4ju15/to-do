7552ea91829da816151ce4fc2c35efb6dedf9b30
n4ju15 <anajulia.garialbano15@gmail.com>
2024-01-26 21:14:40 -0300

    Change the global styles directory in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import GlobalStyles from './GlobalStyles.js'
+import GlobalStyles from './globalStyles.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
